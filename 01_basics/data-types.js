28a5275fc999f74c715dd2dc56d2c7f7a09d4189
Rohit0129 <more29rohit@gmail.com>
2026-08-29 13:28:26 +0530

    added datatypes

---
+++ 01_basics/data-types.js
@@ -0,0 +1,15 @@
+let name = "Rohit"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
