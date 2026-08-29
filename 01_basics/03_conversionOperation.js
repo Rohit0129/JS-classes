e6c53371d9b2116f2a3be5a01b1328950602b75e
Rohit0129 <more29rohit@gmail.com>
2026-08-29 13:42:50 +0530

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+/*
+"33" => 33
+"33abc" => NaN(Not a Number)
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = "rohit";
+
+// let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "rohit" => true
+
+let someNumber = 29;
+let stringNumber = String(someNumber);
+console.log( typeof stringNumber);
