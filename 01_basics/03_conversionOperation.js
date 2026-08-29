86f3bbdd0edd7597b2cc5125200dfa3d6ae53626
Rohit0129 <more29rohit@gmail.com>
2026-08-29 14:03:14 +0530

    ComparrionsOperation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33";
+// let score = "33";
 
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -14,7 +14,7 @@ true => 1; false => 0
 
 */
 
-let isLoggedIn = "rohit";
+// let isLoggedIn = "rohit";
 
 // let booleanLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanLoggedIn);
@@ -24,6 +24,31 @@ let isLoggedIn = "rohit";
 // "" => false
 // "rohit" => true
 
-let someNumber = 29;
-let stringNumber = String(someNumber);
-console.log( typeof stringNumber);
+// let someNumber = 29;
+// let stringNumber = String(someNumber);
+// console.log( typeof stringNumber);
+
+// ***************** Operations **********************
+
+let value = 2;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Rohit";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 3 + 4);
+/* If string is first it will treat every other like string so 134 output
+"1" + 3 => 13
+"13" + 4 => 134
+output => 134
+*/
+
+console.log(1 + 3 + "4");
+/* if string is last it will treat other like number and then string so 44 output
+1 + 3 => 4
+4 + "4" => 44
+output => 44
+*/
