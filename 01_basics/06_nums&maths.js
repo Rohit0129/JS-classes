5db1ac35e52bd1b712a967bffb615f4af63efef3
Rohit0129 <more29rohit@gmail.com>
2026-08-30 16:28:01 +0530

    Math and Numbers done

---
+++ 01_basics/06_nums&maths.js
@@ -0,0 +1,34 @@
+const balance = new Number(300);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber= 123.4567;
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ****************** Maths **********************
+// console.log(Math);
+// console.log(Math.abs(-3)); // Turn any neg value in positive
+// console.log(Math.round(3.5));
+// console.log(Math.ceil(3.1)); //It chooses round top value
+// console.log(Math.floor(3.7)); //It chooses round bottom value
+// console.log(Math.max(3, 4, 6, 7, 9));
+// console.log(Math.min(3, 4, 6, 7, 9));
+console.log(Math.random()); //Returns value between 0 to 1
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
